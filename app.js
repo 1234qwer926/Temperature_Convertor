e31eefcacfd01c66808a66ba67eb061dc6fdc39c
1234qwer926 <srao50872@gmail.com>
2024-08-29 02:47:11 +0530

    last-commit

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ form.addEventListener("submit",function(event){
         Result.value=`${a}°C`;
     }else if((type.value===from.value)&&(type.value==="Fahrenheit")){
             Result.value=`${a}°F`;
-    }else if (from === "Celsius" && type === "Fahrenheit") {
+    }else if (from.value === "Celsius" && type.value === "Fahrenheit") {
         Result.value = `${(a * 9/5) + 32}°F`;
-    } else if (from === "Fahrenheit" && type === "Celsius") {
-        Result.value = `${((a - 32) * 5/9).toFixed(2)}°C`;
+    } else if (from.value === "Fahrenheit" && type.value === "Celsius") {
+        Result.value = `${((a - 32) * 5/9)}°C`;
     }
 })
